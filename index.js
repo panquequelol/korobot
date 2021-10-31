f33288040cf7189f9726dc8c6ca45fc69dc634f2
Rene <rene@outlook.my>
2021-10-31 20:58:26 -0300

    Update index.js

--- index.js
+++ index.js
@@ -72,8 +72,8 @@ client.on('messageCreate', (message) => {
 						}`,
 						link: `https://www.youtube.com/watch?v=${vtuber.yt_video_key}`,
 						titulo: vtuber.title,
-						miniatura: vtuber.thumbnail,
-						viewers: vtuber.live_viewers,
+						imagen: vtuber.thumbnail,
+						// viewers: vtuber.live_viewers,
 					}));
 
 					message.reply(` > ** Están en stream... **`);
@@ -81,7 +81,7 @@ client.on('messageCreate', (message) => {
 						let embedTitulo = new MessageEmbed()
 							.setTitle(vtuber.titulo)
 							.setURL(vtuber.link)
-							.setAuthor(`${vtuber.nombre} ${diaStream}/${mesStream}`)
+							.setAuthor(`${vtuber.nombre}`)
 							.setImage(vtuber.imagen);
 
 						message.channel.send({ embeds: [embedTitulo] });
